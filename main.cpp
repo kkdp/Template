a4e9c2d99545a70ef3dd6eed44555814caeaa8c6
kkdp <35932254+kkdp@users.noreply.github.com>
2018-03-06 20:59:52 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+        #include <iostream>
+        #include <cstdio>
+        #include <vector>
+        #include <set>
+        #include <climits>
+        #include <map>
+        #include <algorithm>
+        #include <cmath>
+        #include <utility>
+        #include <string>
+        #include <cstring>
+        #include <cstdio>
+        #include <iomanip> // setprecision()
+
+        using namespace std;
+        #define ll long long int
+        #define pb push_back
+        #define mp make_pair
+        #define ull unsigned long long int
+         
+        int main()
+        {
+            #ifndef ONLINE_JUDGE
+            freopen("inp.txt","r",stdin);
+            freopen("out.txt", "w", stdout);
+            #endif
+            
+        } 
